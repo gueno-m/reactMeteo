b9bb0d49141e749452793157d06c3e1560d9cc75
gueno-m <gueno-m@users.noreply.github.com>
2020-03-19 13:20:13 +0100

    Stylisation de la page Home.jsx

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View} from 'react-native';
 import Home from './components/Home';
 
 export default function App() {
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,8 +1,82 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
+import soleil from '../assets/soleil.gif';
+
+const styles = StyleSheet.create({
+
+    contenu: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+
+    localisation: {
+        width: 'auto',
+        height: 'auto',
+        flex: 2,
+        flexDirection: 'column',
+        margin: 'auto',
+        marginTop: 50,
+        marginRight: 0,
+        marginBottom: 0,
+    },
+
+    date: {
+        textAlign: 'right',
+        margin: 'auto',
+        color: 'white',
+        fontSize: 20,
+    },
+
+    ville: {
+        textAlign: 'right',
+        margin: 'auto',
+        color: 'white',
+        fontSize: 35,
+        fontWeight: 'bold',
+    },
+
+    heure: {
+        textAlign: 'right',
+        margin: 'auto',
+        color: 'white',
+        fontSize: 20,
+    },
+
+    image: {
+        margin: 'auto',
+        marginTop: -50,
+        width: 350,
+        height: 350,
+    },
+
+    description: {
+        textAlign: 'center',
+        margin: 'auto',
+        color: 'white',
+        fontSize: 30,
+    },
+
+    temperature: {
+        textAlign: 'center',
+        margin: 'auto',
+        color: 'white',
+        fontSize: 80,
+    },
+});
 
 export default function Home() {
     return (
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.contenu}>
+            <View style={styles.localisation}>
+                <Text style={styles.date}>Jeudi 19 Mars</Text>
+                <Text style={styles.ville}>THIAIS</Text>
+                <Text style={styles.heure}>12:30</Text>
+            </View>
+            <Text style={styles.description}>Ensoleillé</Text>
+            <Image style={styles.image}
+                source={soleil}
+            />
+            <Text style={styles.temperature}>19°C</Text>
+        </View>
     );
-  }
+}
